874f335c750f20e50419b09b4778f641427c3901
GradiM <gradi.mandambu@outlook.com>
2019-01-16 22:21:33 +0100

    Début de la Homepage

--- src/app/display-infos/display-infos.component.ts
+++ src/app/display-infos/display-infos.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -9,31 +8,32 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DisplayInfosComponent implements OnInit {
 
-	@Input() user: object;
-	username:string;
-	imgsrc = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4Srcvn8YBPQROuYpi2Nz3CI8H2LS4zuMDORp0qrcf19qeShHukQ';
-	todoList: object;
-	isLoaded = false;
+  @Input() user: object;
+  imagesrc = 'https://www.papillesetpupilles.fr/wp-content/uploads/2016/09/Poulet-a-la-courge-butternut.jpg';
+  username: string;
+  playlists = ['Année 2000', 'Année 90', 'Juste pour rire', 'Top 2018'];
+  todoList: object;
+  isLoaded = false;
 
-  constructor(private http: HttpClient) { 
-  	this.username='Dylan';
+  constructor(private http: HttpClient) {
+    this.username = 'Gradi';
   }
-	playlists=['Année 2000','Année 90','Juste pour rire','Top 2018'];
-
-	addPlaylist(playlist: string) {
-	this.playlists.push(playlist);
-	}
 
   ngOnInit() {
-  	this.http
-  		.get('https://jsonplaceholder.typicode.com/todos/1')
-  		.subscribe(data => {
-    		this.todoList = data;
-    		this.isLoaded = true;
+    this.http
+      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .subscribe(
+        data => {
+          this.todoList = data;
+          this.isLoaded = true;
         },
         error => {
-        	console.log(error);
+          console.log(error);
         }
-      );  	
+      );
+  }
+
+  addPlaylist(playlist: string) {
+    this.playlists.push(playlist);
   }
 }
