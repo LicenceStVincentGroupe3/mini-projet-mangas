c41bb16713789cda332e120c686ed7b9cee8dd4c
GradiM <gradi.mandambu@outlook.com>
2019-01-22 16:16:36 +0100

    Ajout du design du menu + barre de recherche

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-menu-site',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  imageLogo = "../assets/img/logo-site.png";
+
   constructor() { }
 
   ngOnInit() {
+    // Animation de menu lorsqu'en  faisant un sroll en qui quitte le sommet de la page
+    $(function(){
+      var navbar = $('.navbar');
+
+      $(window).scroll(function(){
+        if($(window).scrollTop() <= 40){
+          navbar.removeClass('navbar-scroll');
+
+          navbar.addClass('taille-text-avant-scroll');
+          navbar.removeClass('taille-text-apres-scroll');
+        }
+        else {
+          navbar.addClass('navbar-scroll');
+
+          navbar.addClass('taille-text-apres-scroll');
+          navbar.removeClass('taille-text-avant-scroll');
+        }
+      });
+    });
   }
 
 }
