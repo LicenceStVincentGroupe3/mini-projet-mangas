53a8483cda5fc143546a5b7d14b2ac6b00a89d39
GradiM <gradi.mandambu@outlook.com>
2019-03-11 00:00:06 +0100

    Allègement du code x2

--- src/app/shared/directives/lazy-scroll/lazy-scroll.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LazyScrollDirective } from './lazy-scroll.directive';
-
-describe('LazyScrollDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LazyScrollDirective();
-    expect(directive).toBeTruthy();
-  });
-});
